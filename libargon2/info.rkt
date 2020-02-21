bc166ce91304e5ea1fd81e88eb41e125be3ccf99
Bogdan Popa <bogdan@defn.io>
2020-02-21 11:09:32 +0200

    core: add libargon2 metapackage

---
+++ libargon2/info.rkt
@@ -0,0 +1,8 @@
+#lang info
+
+(define version "20190702")
+(define deps '("base"
+               ("libargon2-i386-win32" #:platform #rx"win32\\i386")
+               ("libargon2-x86_64-linux" #:platform #rx"x86_64-linux")
+               ("libargon2-x86_64-macosx" #:platform #rx"x86_64-macosx")
+               ("libargon2-x86_64-win32" #:platform #rx"win32\\x86_64")))
