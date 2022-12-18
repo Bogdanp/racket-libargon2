b325c05fcc2964958220f7012214f5a04d8f80f1
Bogdan Popa <bogdan@defn.io>
2022-12-18 13:01:15 +0200

    bind: update Visual Studio paths for windows-2019

--- bin/build-win32-i386.cmd
+++ bin/build-win32-i386.cmd
@@ -3,6 +3,6 @@
 git submodule update --init
 mkdir artifacts\win32-i386
 cd argon2
-"%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSbuild.exe" Argon2.sln /p:Configuration=Release /p:Platform=x86 /m
+"%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSbuild.exe" Argon2.sln /p:Configuration=Release /p:Platform=x86 /m
 copy vs2015\build\Argon2RefDll.dll ..\artifacts\win32-i386\libargon2.dll
 cd ..
--- bin/build-win32-x86-64.cmd
+++ bin/build-win32-x86-64.cmd
@@ -3,6 +3,6 @@
 git submodule update --init
 mkdir artifacts\win32-x86-64
 cd argon2
-"%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSbuild.exe" Argon2.sln /p:Configuration=Release /p:Platform=x64 /m
+"%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSbuild.exe" Argon2.sln /p:Configuration=Release /p:Platform=x64 /m
 copy vs2015\build\Argon2OptDll.dll ..\artifacts\win32-x86-64\libargon2.dll
 cd ..
